0332761cb5251c4cc693346acaea390cb44327c5
simonHuang12 <simonh8@nycstudents.net>
2022-09-29 09:27:11 -0400

    2

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class CatRunner {
+public class CatRunner {
+    public static void main(String[] args)
+    {
+        Cat cat1 = new Cat("Bob", 12, 6.2);
+        cat1.introduce();
+
+        Cat cat2 = new Cat("Joe", 12412, 0.2);
+        cat2.introduce();
+        cat2.printCatInfo();
+    }
 }
